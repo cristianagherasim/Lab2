2acebae9d442ccf796a8d96370d379e432282f26
Cristiana Pelean <cristianapelean@Cristianas-MacBook.local>
2022-10-13 16:23:03 -0400

    Lab2

---
+++ refresher.js
@@ -0,0 +1,21 @@
+let firstName="Cristiana";
+let lastName="Gherasim";
+let fullName=firstName.concat(lastName);
+console.log(fullName);
+
+let population=639111;
+console.log(population);
+population *= '3';
+console.log(population *= '3');
+
+let javaScriptIsCool = true; 
+let colors = ["red","orange","yellow","green","blue","indigo","violet"];
+
+
+for (var i = 1; i < 6; i++){
+    console.log(i);
+  }
+
+for (var i = 0; i < 5; i++){
+  console.log(i);
+}
